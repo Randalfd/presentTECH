41b9be4cdef2d6524b0dc53c4fd51e41724c9343
Santiago Gutierrez <santiago.gutierrez@ir3.local>
2024-06-06 07:59:03 -0300

    feat: indent

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,31 +4,33 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        echo view('home');
-        echo view('footer');
-    }
+	public function index()
+	{
+		echo view('header');
+		echo view('home');
+		echo view('footer');
+	}
 
-    public function logohome()
-    {
-        echo view('home');
-        echo view('footer');
-    }
+	public function logohome()
+	{
+		echo view('home');
+		echo view('footer');
+	}
 
-    public function home_login()
-    {
-        echo view('login');
-    }
+	public function home_login()
+	{
+		echo view('login');
+	}
 
-    public function home_register()
-    {
-        echo view('register');
-    }
+	public function home_register()
+	{
+		echo view('register');
+	}
 
-    public function after_login()
-    {
-        echo view('loginhome');
-        echo view('footer');
-    }
+	public function after_login()
+	{
+		echo view('header');
+		echo view('loginhome');
+		echo view('footer');
+	}
 }
